e21723d55f701140922b092afb88d885c18db87f
andrea.moschella <andrea.moschella@nurale.cpom>
2023-04-18 15:57:45 +0200

    feat: added customers

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,8 +1,11 @@
+import Button from "../Button";
+
 interface Props {
   columns: { name: string; columnName: string }[];
   data: any[];
+  handleEdit: (id: number)=>void
 }
-const Table = ({ columns, data }: Props) => {
+const Table = ({ columns, data, handleEdit }: Props) => {
   return (
     <table className="min-w-full divide-y divide-gray-300">
       <thead className="bg-gray-50">
@@ -18,16 +21,21 @@ const Table = ({ columns, data }: Props) => {
         </tr>
       </thead>
       <tbody className="divide-y divide-gray-200 bg-white">
-        {data.map((user) => (
-          <tr key={user.id}>
+        {data.map((data) => (
+          <tr key={data.id}>
             {columns.map((column, index) => (
               <td
                 key={index + column.columnName}
                 className="whitespace-nowrap px-3 py-4 text-sm text-gray-500"
               >
-                {user[column.columnName]}
+                {data[column.columnName]}
               </td>
             ))}
+              <td
+                  className="whitespace-nowrap px-3 py-4 text-sm text-gray-500"
+              >
+                  <Button onClick={()=>handleEdit(data.id)}>Edit</Button>
+              </td>
           </tr>
         ))}
       </tbody>

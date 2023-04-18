41fd20bcf0b98a479b318ac7bdbb65634dafe7e3
andrea.moschella <andrea.moschella@nurale.cpom>
2023-04-18 12:55:30 +0200

    feat: added table

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,28 +1,38 @@
-interface Props{
-    columns: {name: string, columnName: string}[]
-    data: any[]
-
+interface Props {
+  columns: { name: string; columnName: string }[];
+  data: any[];
 }
-const Table = ({columns, data}:Props)=>{
-
-    return  <table className="min-w-full divide-y divide-gray-300">
-        <thead className="bg-gray-50">
+const Table = ({ columns, data }: Props) => {
+  return (
+    <table className="min-w-full divide-y divide-gray-300">
+      <thead className="bg-gray-50">
         <tr>
-            {columns.map(column=><th className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-                {column.name}
-            </th>)}
+          {columns.map((column) => (
+            <th
+              key={column.columnName}
+              className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+            >
+              {column.name}
+            </th>
+          ))}
         </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-200 bg-white">
-        {data.map(user => <tr key={user.id}>{
-            columns.map((column)=>
-                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                    {user[column.columnName]}
-                </td>)
-        }
-        </tr>)}
-        </tbody>
+      </thead>
+      <tbody className="divide-y divide-gray-200 bg-white">
+        {data.map((user) => (
+          <tr key={user.id}>
+            {columns.map((column, index) => (
+              <td
+                key={index + column.columnName}
+                className="whitespace-nowrap px-3 py-4 text-sm text-gray-500"
+              >
+                {user[column.columnName]}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
     </table>
-}
+  );
+};
 
-export default Table
+export default Table;
